4da7cee739cf538f6f1779b43828e1e28c177db3
Rock Lee <rockdotlee@gmail.com>
2017-06-11 23:54:26 +0800

    list: Change the list previous pointer name.
    Signed-off-by: Rock Lee <rockdotlee@gmail.com>

--- include/linux/list.h
+++ include/linux/list.h
@@ -4,45 +4,45 @@
 #include <linux/types.h>
 
 #define LIST_HEAD(name) \
-		struct list_head name = {.pre = &(name), .next = &(name)}
+		struct list_head name = {.prev = &(name), .next = &(name)}
 
 #define LIST_HEAD_INIT(head) \
 	do {	\
-		head->pre = head;	\
+		head->prev = head;	\
 		head->next = head;	\
 	} while (0)
 
 static inline void list_init(struct list_head *head)
 {
 	head->next = head;
-	head->pre = head;
+	head->prev = head;
 }
 
 static inline void list_add_tail(struct list_head *new, struct list_head *head)
 {
 	new->next = head;
-	new->pre = head->pre;
-	head->pre->next = new;
-	head->pre = new;
+	new->prev = head->prev;
+	head->prev->next = new;
+	head->prev = new;
 }
 
 static inline void list_add(struct list_head *new, struct list_head *head)
 {
 	new->next = head->next;
-	new->pre = head;
-	head->next->pre = new;
+	new->prev = head;
+	head->next->prev = new;
 	head->next = new;
 }
 
 static inline void list_del(struct list_head *entry)
 {
-	struct list_head *pre = entry->pre;
+	struct list_head *prev = entry->prev;
 	struct list_head *next = entry->next;
 
-	pre->next = next;
-	next->pre = pre;
+	prev->next = next;
+	next->prev = prev;
 
-	entry->pre = 0;
+	entry->prev = 0;
 	entry->next = 0;
 }
 
