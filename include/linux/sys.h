0c32d6db9bc7d661f26c9a49def162d6b48b4975
BuddyZhang1 <buddy.d.zhang@gmail.com>
2017-08-05 16:05:59 +0800

    add new system call: sys_close: Closes a file on the host system

--- include/linux/sys.h
+++ include/linux/sys.h
@@ -4,6 +4,7 @@ extern int sys_fork();
 extern int sys_read();
 extern int sys_write();
 extern int sys_open();
+extern int sys_close();
 
 fn_ptr sys_call_table[] = {
 sys_setup, /* system setup */
@@ -12,4 +13,5 @@ sys_fork,  /* create a child process */
 sys_read,  /* read from a file descriptor */
 sys_write, /* write to a file descriptor */
 sys_open, /* open and possibly create a file */
+sys_close, /* closes a file on the host system */
 };
