6f75330c303bdb9e949ab37cddf853bb0415ae32
BuddyZhang1 <buddy.d.zhang@gmail.com>
2017-08-12 09:48:22 +0800

    add system call - sys_prof : profiling library

--- include/linux/sys.h
+++ include/linux/sys.h
@@ -42,6 +42,7 @@ extern int sys_rmdir();
 extern int sys_dup();
 extern int sys_pipe();
 extern int sys_times();
+extern int sys_prof();
 extern int sys_setreuid();
 
 fn_ptr sys_call_table[] = {
@@ -89,5 +90,6 @@ sys_rmdir, /* Remove a directory */
 sys_dup, /* duplicate an open file descriptor */
 sys_pipe, /* create an interprocess channel */
 sys_times, /* file access and modification times structure */
+sys_prof, /* profiling library */
 sys_setreuid, /* set real and/or effective user or group ID */
 };
