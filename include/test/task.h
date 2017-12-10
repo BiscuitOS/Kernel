1872a1b6064b45631faa98493911e5176389620f
BuddyZhang1 <buddy.d.zhang@gmail.com>
2017-12-11 00:43:43 +0800

    Debug 1/2: segment

--- include/test/task.h
+++ include/test/task.h
@@ -7,4 +7,8 @@ extern int test_task_scheduler(void);
 extern void debug_gdt_common(void);
 #endif
 
+#ifdef CONFIG_TESTCASE_SEGMENT
+extern void debug_segment_common(void);
+#endif
+
 #endif
---
+++ tools/testcase/scheduler/segment/segment.c
@@ -0,0 +1,12 @@
+/*
+ * Segment
+ */
+#include <linux/kernel.h>
+
+#include <test/debug.h>
+
+/* segment common entry */
+void debug_segment_common(void)
+{
+    printk("Hello World\n");
+}
