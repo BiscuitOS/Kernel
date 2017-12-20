b7c2eca8a036883618831f45f5a73d9a70b6c4ba
BuddyZhang1 <buddy.zhang@aliyun.com>
2017-12-20 13:55:06 +0800

    CMOS RAM: RTC and Organization of CMOS Memory - Clock

--- tools/testcase/timer/cmos/cmos.c
+++ tools/testcase/timer/cmos/cmos.c
@@ -17,6 +17,20 @@
 
 #define BCD_TO_BIN(val)  ((val)=((val) & 15) + ((val) >> 4) * 10)
 
+/* Data fomat for RTC */
+enum
+{
+    F_BCD,
+    F_BINARY
+};
+
+/* Hour format for RTC */
+enum
+{
+    H_12H,
+    H_24H
+};
+
 /*
  * Organization of CMOS Memory - Clock
  *   00 -- 0E is defined by the clock hardware and all must follow it.
@@ -68,8 +82,76 @@
  *   CMOS 06h --- RTC - DAY OF WEEK
  *   Desc: 01 - 07 Sunday = 1
  *   SeeAlso: CMOS 07h, CMOS 08h, CMOS 09h
- */
-
+ *
+ *   ----- R07 -----------------------------------------------
+ *   CMOS 07h --- RTC - DATE of MONTH
+ *   Desc: BCD 01 - 31, HEX 01 - 1F
+ *   SeeAlso: CMOS 06h, CMOS 08h, CMOS 09h
+ *
+ *   ----- R08 -----------------------------------------------
+ *   CMOS 08h --- RTC - MONTH
+ *   Desc: BCD 01 - 12, HEX 01 - 0C
+ *   SeeAlso: CMOS 06h, CMOS 07h, CMOS 09h
+ *
+ *   ----- R09 -----------------------------------------------
+ *   CMOS 09h --- RTC - YEAR
+ *   Desc: BCD 00 - 99, HEX 00 - 63
+ *   Notes: BCD/HEX selection depends on Bit 2 of register B (0Bh)
+ *          12/24 Hr selection depends on Bit 1 of register B (0Bh)
+ *          Alarm will trigger when contents of all three Alarm byte 
+ *          registers match their companions.
+ *   SeeAlso: CMOS 06h, CMOS 07h, CMOS 08h
+ *  
+ *   ----- R0A -----------------------------------------------
+ *   CMOS 0Ah --- RTC - STATUS REGISTER A (read/write)
+ *   Bitfields for RTC Status Register A:
+ *   Bit(s)  Description   (Table C001)
+ *     7     = 1 time update cycle in progress, data output undefined
+ *           (bit 7 is read only)
+ *     6-4   22 stage divider
+ *           010 = 32768 Hz time base (default)
+ *     3-0   rate selection bits for interrupt
+ *           0000 none
+ *           0011 122 microseconds (minimum)
+ *           1111 500 milliseconds
+ *           0110 976.562 milliseconds (default 1024 Hz)
+ *   SeeAlso: #C002, #C003, #C004
+ *
+ *   ----- R0B -----------------------------------------------
+ *   CMOS 0Bh --- RTC - STATUS REGISTER B (read/write)
+ *   Bitfields for RTC Status Register B: 
+ *   Bit(s)  Description   (Table C002)
+ *     7     Enable cycle update
+ *     6     Enable periodic interrupt
+ *     5     Enable alarm interrupt
+ *     4     Enable update-enable interrupt
+ *     3     Enable square wave 
+ *     2     Data Mode - 0: BCD, 1: Binary
+ *     1     24/12 hour selection - 1 enables 24 hour mode
+ *     0     Daylight Saving Enable - 1 enables
+ *   SeeAlso: #C001, #C004
+ *
+ *   ----- R0C -----------------------------------------------
+ *   CMOS 0Ch --- RTC - STATUS REGISTER C (Read only)
+ *   Bitfields for RTC Status Register C:
+ *   Bit(s)  Description   (Table C003)
+ *     7     Interrupt request flag
+ *           =1 when any or all of bits 6-4 are 1 and appropriate 
+ *           enables (Register B) are set to 1. Generates IRQ 8 
+ *           when triggered.
+ *     6     Periodic Interrupt flag
+ *     5     Alarm Interrupt flag
+ *     4     Update-Ended Interrupt Flag
+ *     3-0   unused
+ *   SeeAlso: #C001, #C002, #C004
+ *
+ *   ----- R0D -----------------------------------------------
+ *   CMOS 0Dh --- RTC - STATUS REGISTER D (read only)
+ *   Bit(s)  Description     (Table C004)
+ *     7     Valid RAM - 1 indicates batery power good,
+ *                       0 if dead or disconnected.
+ *     6-0   unused
+ */ 
 
 /*
  * Obtain Current Seconds (RTC)
@@ -172,12 +254,220 @@ static unsigned char cmos_RTC_week(void)
     return inb_p(CMOS_DTA);
 }
 
+/*
+ * Obtain Date of Month from RTC
+ *   CMOS 07h --- RTC - DATE of MONTH
+ *   Desc: BCD 01 - 31, HEX 01 - 1F
+ *   SeeAlso: CMOS 06h, CMOS 08h, CMOS 09h
+ *
+ *   @return: the data of month in BCD
+ */
+static unsigned char cmos_RTC_date(void)
+{
+    outb_p(0x80 | 0x07, CMOS_CTR);
+    return inb_p(CMOS_DTA);
+}
+
+/*
+ * Obtain Month from RTC
+ *   CMOS 08h ---- RTC - MONTH
+ *   Desc: BCD 01 - 12, HEX 01 - 0C
+ *   SeeAlso: CMOS 06h, CMOS 07h, CMOS 09h
+ *
+ *   @return: The current month from RCT in BCD
+ */
+static unsigned char cmos_RTC_month(void)
+{
+    outb_p(0x80 | 0x08, CMOS_CTR);
+    return inb_p(CMOS_DTA);
+}
+
+/*
+ * Obtain Year from RTC
+ *   CMOS 09h --- RTC - YEAR
+ *   Desc: BCD 00 - 99, HEX 00 - 63
+ *   Notes: BCD/HEX selection depends on Bit 2 of register B (0Bh)
+ *          12/24 Hr selection depends on Bit 1 of register B (0Bh)
+ *          Alarm will trigger when contents of all three Alarm byte 
+ *          registers match their companions.
+ *   SeeAlso: CMOS 06h, CMOS 07h, CMOS 08h
+ */
+static unsigned char cmos_RTC_year(void)
+{
+    outb_p(0x80 | 0x09, CMOS_CTR);
+    return inb_p(CMOS_DTA);
+}
+
+/*
+ * Obtain RTC Status Register 1
+ *   CMOS 0Ah --- RTC - STATUS REGISTER A (read/write)
+ *   Bitfields for RTC Status register A:
+ *     7     = 1 time update cycle in progress, data output undefined
+ *           (bit 7 is read only)
+ *     6-4   22 stage divider
+ *           010 = 32768 Hz time base (default)
+ *     3-0   rate selection bits for interrupt
+ *           0000 none
+ *           0011 122 microseconds (minimum)
+ *           1111 500 milliseconds
+ *           0110 976.562 milliseconds (default 1024 Hz)
+ *  SeeAlso: #C002, #C003, #C004
+ */
+/* Read Operation */
+static unsigned char cmos_RTC_statusA(void)
+{
+    outb_p(0x80 | 0x0A, CMOS_CTR);
+    return inb_p(CMOS_DTA);
+}
+/* Write Operation */
+static void cmos_RTC_statusA_update(unsigned char state)
+{
+    outb_p(0x80 | 0x0A, CMOS_CTR);
+    outb_p(state, CMOS_DTA);
+}
+
+/*
+ * Obtain RTC Status Register B
+ *   Bitfields for RTC Status Register B: 
+ *   Bit(s)  Description   (Table C002)
+ *     7     Enable cycle update
+ *     6     Enable periodic interrupt
+ *     5     Enable alarm interrupt
+ *     4     Enable update-enable interrupt
+ *     3     Enable square wave 
+ *     2     Data Mode - 0: BCD, 1: Binary
+ *     1     24/12 hour selection - 1 enables 24 hour mode
+ *     0     Daylight Saving Enable - 1 enables
+ *   SeeAlso: #C001, #C004
+ */
+/* Read operation for StatusB */
+static unsigned char cmos_RTC_statusB(void)
+{
+    outb_p(0x80 | 0x0B, CMOS_CTR);
+    return inb_p(CMOS_DTA);
+}
+/* Write operation for StatusB */
+static void cmos_RTC_statusB_update(unsigned char cmd)
+{
+    outb_p(0x80 | 0x0B, CMOS_CTR);
+    outb_p(cmd, CMOS_DTA);
+}
+
+/* 
+ * Setup 24/12 hour
+ *   @mode: 1 - 24 hours
+ *          0 - 12 hours
+ */
+static void setup_hour_mode(int mode)
+{
+    unsigned char status;
+
+    status = cmos_RTC_statusB();
+    /*
+     * Bitfields for RTC Status Register B: 
+     *   Bit(s)  Description   (Table C002)
+     *     7     Enable cycle update
+     *     6     Enable periodic interrupt
+     *     5     Enable alarm interrupt
+     *     4     Enable update-enable interrupt
+     *     3     Enable square wave 
+     *     2     Data Mode - 0: BCD, 1: Binary
+     *     1     24/12 hour selection - 1 enables 24 hour mode
+     *     0     Daylight Saving Enable - 1 enables
+     */
+    status &= 0xFD;
+    status |= (mode & 0x1) << 1;
+    cmos_RTC_statusB_update(status);
+}
+
+/*
+ * Setup Data Format
+ *   @mode: 0 -- BCD
+ *          1 -- Binary
+ */
+static void setup_data_format(unsigned char mode)
+{
+    unsigned char status;
+
+    status = cmos_RTC_statusB();
+    /*
+     * Bitfields for RTC Status Register B: 
+     *   Bit(s)  Description   (Table C002)
+     *     7     Enable cycle update
+     *     6     Enable periodic interrupt
+     *     5     Enable alarm interrupt
+     *     4     Enable update-enable interrupt
+     *     3     Enable square wave 
+     *     2     Data Mode - 0: BCD, 1: Binary
+     *     1     24/12 hour selection - 1 enables 24 hour mode
+     *     0     Daylight Saving Enable - 1 enables
+     */
+    status &= 0xFB;
+    status |= (mode & 0x1) << 2;
+    cmos_RTC_statusB_update(status);
+}
+
+/*
+ * Obtain RTC Status Register C
+ *   CMOS 0Ch --- RTC - STATUS REGISTER C (Read only)
+ *   Bitfields for RTC Status Register C:
+ *   Bit(s)  Description   (Table C003)
+ *     7     Interrupt request flag
+ *           =1 when any or all of bits 6-4 are 1 and appropriate 
+ *           enables (Register B) are set to 1. Generates IRQ 8 
+ *           when triggered.
+ *     6     Periodic Interrupt flag
+ *     5     Alarm Interrupt flag
+ *     4     Update-Ended Interrupt Flag
+ *     3-0   unused
+ *   SeeAlso: #C001, #C002, #C004 
+ */
+static unsigned char cmos_RTC_statusC(void)
+{
+    outb_p(0x80 | 0x0C, CMOS_CTR);
+    return inb_p(CMOS_DTA);
+}
+
+/*
+ * Obtain RTC Status Register D
+ *   CMOS 0Dh --- RTC - STATUS REGISTER D (read only)
+ *   Bit(s)  Description     (Table C004)
+ *     7     Valid RAM - 1 indicates batery power good,
+ *                       0 if dead or disconnected.
+ *     6-0   unused
+ */
+static unsigned char cmos_RTC_statusD(void)
+{
+    outb_p(0x80 | 0x0D, CMOS_CTR);
+    return inb_p(CMOS_DTA);
+}
+
+/*
+ * Obtain power state for CMOS RAM
+ * 
+ *   @return: 1 indicates battery power good..
+ *            0 if dead or disconnected.
+ */
+static int cmos_ram_power_status(void)
+{
+    unsigned char status;
+
+    /*
+     * CMOS 0Dh --- RTC - STATUS REGISTER D (read only)
+     *   Bit(s)  Description     (Table C004)
+     *     7     Valid RAM - 1 indicates batery power good,
+     *                       0 if dead or disconnected.
+     */
+    status = cmos_RTC_statusD();
+    return (status & 0x80) ? 1 : 0;
+}
+
 void debug_cmos_clk_common(void)
 {
     /* Add item */
 
-    /* Ignore warning */
-    if (1) {
+    /* Ignore warning, default usage for CMOS RAM (RTC) */
+    if (0) {
         unsigned char value;
 
         /* Obtain current seconds from RTC */
@@ -201,5 +491,37 @@ void debug_cmos_clk_common(void)
         /* Obtain current week from RTC */
         value = cmos_RTC_week();
         printk("CMOS RTC week %d\n", BCD_TO_BIN(value));
+        /* Obtain date of Month from RTC */
+        value = cmos_RTC_date();
+        printk("CMOS RTC date %d\n", BCD_TO_BIN(value));
+        /* Obtain month from RTC */
+        value = cmos_RTC_month();
+        printk("CMOS RTC month %d\n", BCD_TO_BIN(value));
+        /* Obtain year from RTC */
+        value = cmos_RTC_year();
+        printk("CMOS RTC year %d\n", BCD_TO_BIN(value));
+        /* Obtain RTC Status Register A */
+        value = cmos_RTC_statusA();
+        cmos_RTC_statusA_update(value);
+        printk("CMOS RTC StatusA %#X\n", value);
+        /* Obtain RTC Status Register B */
+        value = cmos_RTC_statusB();
+        printk("CMOS RTC StatusB %#x\n", value);
+        /* Setup 24-hour, Binary format */
+        setup_hour_mode(H_12H);
+        setup_data_format(F_BINARY);
+        value = cmos_RTC_hour();
+        printk("CMOS RTC Hour(12h/Binary) %d\n", value);
+        /* Obtain RTC Status Register C */
+        value = cmos_RTC_statusC();
+        printk("CMOS RTC StatusC %#x\n", value);
+        /* Obtain RTC Status Register D */
+        value = cmos_RTC_statusD();
+        printk("CMOS RTC StatusD %#x\n", value);
+        /* Obtain CMOS RAM Power Status */
+        if (cmos_ram_power_status())
+            printk("CMOS RAM Power On.\n");
+        else
+            printk("CMOS RAM Dead or Disconnect.\n");
     }
 }
