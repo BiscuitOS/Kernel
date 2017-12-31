bf6b5218e1e95aba5d5f243b04bd6c1faa223c23
BuddyZhang1 <buddy.d.zhang@gmail.com>
2017-12-31 08:54:26 +0800

    system-call: Basic routine for invoke system from userland to kernel

--- tools/testcase/systemcall/common.c
+++ tools/testcase/systemcall/common.c
@@ -2,6 +2,10 @@
  * Common System Call
  *
  * (C) 2017.12 BiscuitOS <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  */
 #include <linux/kernel.h>
 
@@ -9,6 +13,11 @@
 
 int common_system_call_entry(void)
 {
-    printk("Hello World\n");
+    printk("Common system call\n");
+
+#ifdef CONFIG_TESTCASE_SYSCALL_ROUTINE
+    common_system_call_rountine();
+#endif
+
     return 0;
 }
