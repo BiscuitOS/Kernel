5df302fa66d06971ed8af071e1baa36ce9f84cd5
BuddyZhang1 <buddy.zhang@aliyun.com>
2018-03-01 19:15:29 +0800

    2/4 MINIXFS: inode mechanism

--- tools/testcase/vfs/minixfs/inode.c
+++ tools/testcase/vfs/minixfs/inode.c
@@ -8,9 +8,11 @@
  * published by the Free Software Foundation.
  */
 #include <linux/kernel.h>
-#include <linux/hdreg.h>
 #include <linux/fs.h>
+#include <linux/hdreg.h>
 #include <linux/mm.h>
+
+#include <sys/stat.h>
 
 #include <test/debug.h>
 
@@ -43,6 +45,148 @@
  *   is included via <sys/types.h>. Below is a list of octal numbers which
  *   can be extracted from the mode entry. This information can also be found
  *   in the stat(2) and chmod(2) man page.
+ */
+
+/*
+ * Inode MODE
+ *
+ *   i_mode is used to store file type and access permission attribute.
+ *   And i_mode[15:12] is used to store file type, i_mode[11:9] is used
+ *   to hold setting information while it is modified. i_mode[8:0] is used
+ *   to hold access permission. e.g. figure. More information see 
+ *   'include/sys/stat.h'
+ *
+ *   15--------12-11------------------9-8---------------------0
+ *   | file type | setting information | access permision     |
+ *   ----------------------------------------------------------
+ *
+ *   i_mode[15:12] File Type
+ *   ----------------------------------------------------------
+ *   | Value      | Describe                                  |
+ *   ----------------------------------------------------------
+ *   | 000        |                                           |
+ *   ---------------------------------------------------------- 
+ *   | 001        | FIFO/Named Pipe                           |
+ *   ---------------------------------------------------------- 
+ *   | 010        | Character Device                          |
+ *   ---------------------------------------------------------- 
+ *   | 011        |                                           |
+ *   ---------------------------------------------------------- 
+ *   | 100        | Dirent                                    |
+ *   ---------------------------------------------------------- 
+ *   | 101        |                                           |
+ *   ---------------------------------------------------------- 
+ *   | 110        | Block Device                              |
+ *   ---------------------------------------------------------- 
+ *   | 111        | Regular File                              |
+ *   ---------------------------------------------------------- 
+ *    
+ *   i_mode[11:9] Setting inforamtion
+ *   ----------------------------------------------------------
+ *   | Value      |                                           |
+ *   ----------------------------------------------------------
+ *   | 001        | set-user-ID                               |
+ *   ----------------------------------------------------------
+ *   | 010        | set-group-ID                              |
+ *   ----------------------------------------------------------
+ *   | 100        | Constrained delete flag (only for Dirent) |
+ *   ----------------------------------------------------------
+ *
+ *   i_mode[8:6] User Access Permission
+ *   ----------------------------------------------------------
+ *   | Value      |                                           |
+ *   ----------------------------------------------------------
+ *   | R [8]      | Read permision                            |
+ *   ----------------------------------------------------------
+ *   | W [7]      | Write permision                           |
+ *   ----------------------------------------------------------
+ *   | X [6]      | Execute permision                         |
+ *   ----------------------------------------------------------
+ *
+ *   i_mode[5:3] Group Access Permission
+ *   ----------------------------------------------------------
+ *   | Value      |                                           |
+ *   ----------------------------------------------------------
+ *   | R [5]      | Read permision                            |
+ *   ----------------------------------------------------------
+ *   | W [4]      | Write permision                           |
+ *   ----------------------------------------------------------
+ *   | X [3]      | Execute permision                         |
+ *   ----------------------------------------------------------
+ *
+ *   i_mode[2:0] User Access Permission
+ *   ----------------------------------------------------------
+ *   | Value      |                                           |
+ *   ----------------------------------------------------------
+ *   | R [2]      | Read permision                            |
+ *   ----------------------------------------------------------
+ *   | W [1]      | Write permision                           |
+ *   ----------------------------------------------------------
+ *   | X [0]      | Execute permision                         |
+ *   ----------------------------------------------------------
+ *
+ */
+static void parse_inode_mode(struct m_inode *inode)
+{
+    int mode = inode->i_mode;
+
+    if (S_ISREG(mode))
+        printk("File Type: Regular file\n");
+    else if (S_ISDIR(mode))
+        printk("File Type: Directory\n");
+    else if (S_ISCHR(mode))
+        printk("File Type: Character Device\n");
+    else if (S_ISBLK(mode))
+        printk("File Type: Block Device\n");
+    else if (S_ISFIFO(mode))
+        printk("File Type: FIFO/Named Pipe\n");
+    else if (S_ISSOCK(mode))
+        printk("File Type: Socket\n");
+    else if (S_ISLNK(mode))
+        printk("File Type: Symoble link\n");
+    else
+        printk("File Type: Unknown type\n");
+
+
+    /* Setting information upon execution */
+    if (mode & S_ISUID)
+        printk("Set Info:  Set user id upon execution\n");
+    if (mode & S_ISGID)
+        printk("Set Info:  Set group id upon execution\n");
+    if (mode & S_ISVTX)
+        printk("Set Info:  Sticky bit\n");
+
+    /* User Access Permission */
+    printk("User  Permission: ");
+    if (mode & S_IRUSR)
+        printk("R ");
+    if (mode & S_IWUSR)
+        printk("W ");
+    if (mode & S_IXUSR)
+        printk("X ");
+
+    /* Group Access Permission */
+    printk("\nGroup Permission: ");
+    if (mode & S_IRGRP)
+        printk("R ");
+    if (mode & S_IWGRP)
+        printk("W ");
+    if (mode & S_IXGRP)
+        printk("X ");
+
+    /* World/Other Permission */
+    printk("\nOther Permission: ");
+    if (mode & S_IROTH)
+        printk("R ");
+    if (mode & S_IWOTH)
+        printk("W ");
+    if (mode & S_IXOTH)
+        printk("X ");
+    printk("\n");
+}
+
+/*
+ * Parse Inode information from MINIXFS.
  *
  *   Scheme of Inode
  *
@@ -96,50 +240,74 @@
  *   | 0x1E     | ZONE 8                                              |
  *   | 0x1F     |                                                     |
  *   ------------------------------------------------------------------
- * 
  */
+static void parse_inode_info(struct m_inode *inode)
+{
+    printk("=============INODE============\n");
+    /* Inode mode */
+    parse_inode_mode(inode);
+    /* UserID */
+    printk("UserID:           %#x\n", inode->i_uid);
+    /* Length of file */
+    printk("Length:           %#x\n", inode->i_size);
+    /* Modify time (from 1970.1.1:0)*/
+    printk("Modify Time:      %#x\n", inode->i_mtime);
+    /* Group ID */
+    printk("GroupID:          %#x\n", inode->i_gid);
+    /* Links number */
+    printk("Link numbers:     %#x\n", inode->i_nlinks);
+    printk("==============================\n");
+}
 
-/* MINIXFS ROOT */
-#define MINIXFS_DEV     0x301
-/* SUPER BLOCK: 2nd block */
-#define SUPER_BLOCK     0x01
-
-/* get super block */
-static struct super_block *get_super_block(void)
+/*
+ * Load special inode from MINIXFS
+ *
+ *   Inode locate in Inode block that contain all inode structure,
+ *    
+ */
+static struct m_inode *read_inode(int root, int ino)
 {
     struct super_block *sb;
     struct buffer_head *bh;
+    struct m_inode *inode = NULL;
+    int block;
 
-    bh = bread(MINIXFS_DEV, SUPER_BLOCK);
-    if (!bh) {
-        printk("MINIXFS: Unable to obtain SuperBlock\n");
-        return NULL;
-    }
-    sb = (struct super_block *)get_free_page();
-    *((struct d_super_block *)sb) = *((struct d_super_block *)bh->b_data);
+    /* Obtain super block */
+    if(!(sb = get_super(root)))
+        panic("trying read super block");
 
+    /* compute the block */
+    block  = 2 + sb->s_imap_blocks + sb->s_zmap_blocks;
+    block += (ino - 1) / INODES_PER_BLOCK;
+    printk("block %#x\n", block);
+
+    /* load logical block from disk */
+    if (!(bh = bread(root, block)))
+        panic("trying read inode block");
+
+    inode = (struct m_inode *)get_free_page();
+    /* Obtain special inode */
+    *(struct d_inode *)inode = ((struct d_inode *)bh->b_data)
+                               [(ino - 1) % INODES_PER_BLOCK];
     brelse(bh);
-    if (sb->s_magic != SUPER_MINIX_MAGIC &&
-        sb->s_magic != SUPER_MINIX_MAGIC_V1) {
-        printk("MINIXFS: Incorrect MAGIC\n");
-        free_page((unsigned long)sb);
-        return NULL;
-    }
-    return sb;
+    return inode;
 }
 
-/* put super block */
-static void put_super_block(struct super_block *sb)
-{
-    free_page((unsigned long)sb);
-}
 
 int debug_vfs_minixfs_inode_userland(void)
 {
     if (1) {
-        put_super_block(get_super_block());
+        struct m_inode *inode;
+
+        inode = read_inode(ROOT_DEV, ROOT_INO);
+        parse_inode_info(inode);
+        free_page((unsigned long)inode);
     } else {
-        put_super_block(get_super_block());
+        struct m_inode *inode;
+
+        inode = read_inode(ROOT_DEV, ROOT_INO);
+        parse_inode_info(inode);
+        free_page((unsigned long)inode);
     }
     return 0;
 }
