947e05224cfa59574c147a5b5089b1b6c506b9bf
BuddyZhang1 <buddy.zhang@aliyun.com>
2018-04-25 18:48:52 +0800

    get_base()? unable to obtain correct base address     for special segment descriptor
      When upgrade to 0.12, we can't obtain correct base address
      from special segment that utilize 'get_base()'? The code
      is bad operation...
    
      We back to 0.11 for 'get_base()' and it could obtain correct
      base address..

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -260,17 +260,17 @@ __asm__("movw %%dx,%0\n\t" \
 #define set_base(ldt,base) _set_base( ((char *)&(ldt)) , base )
 #define set_limit(ldt,limit) _set_limit( ((char *)&(ldt)) , (limit-1)>>12 )
 
-#define _get_base(addr) ({\
-unsigned long __base; \
-__asm__("movb %3,%%dh\n\t" \
-	"movb %2,%%dl\n\t" \
-	"shll $16,%%edx\n\t" \
-	"movw %1,%%dx" \
-	:"=d" (__base) \
-	:"m" (*((addr)+2)), \
-	 "m" (*((addr)+4)), \
-	 "m" (*((addr)+7))); \
-__base;})
+static inline unsigned long _get_base(char *addr)
+{
+    unsigned long __base;
+
+    __asm__("movb %3, %%dh\n\t"
+            "movb %2, %%dl\n\t"
+            "shll $16, %%edx\n\t"
+            "movw %1, %%dx":"=&d"(__base)
+            : "m"(*((addr) + 2)), "m"(*((addr) + 4)), "m"(*((addr) + 7)));
+    return __base;
+}
 
 #define get_base(ldt) _get_base( ((char *)&(ldt)) )
 
